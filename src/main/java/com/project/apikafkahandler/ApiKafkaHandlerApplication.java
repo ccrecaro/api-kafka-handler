5d5e3cf648beed77de9e9e7c10e1f81e5de44115
Carolina Contreras <ccre.caro@gmail.com>
2024-12-30 01:09:45 -0300

    Basic structure

---
+++ src/main/java/com/project/apikafkahandler/ApiKafkaHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.project.apikafkahandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKafkaHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKafkaHandlerApplication.class, args);
+	}
+
+}
